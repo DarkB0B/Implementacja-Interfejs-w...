5cef2f336efd5c41bde72e30a673f41f86972e28
DarkB0B <miecze123@onet.pl>
2022-04-21 22:16:26 +0200

    Zadanie3: MultiDevice working; TO DO: tests

---
+++ Zadanie 3/Fax.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_3
+{
+    internal class Fax: BaseDevice
+    {
+        public int FaxCounter = 0;
+        public void SendFax(in IDocument document, string recipient)
+        {
+            if (IDevice.State.on == state)
+            {
+                DateTime dateTime = DateTime.Now;
+                Console.WriteLine("{0} Fax has been sent to {1}", dateTime, recipient);
+                FaxCounter++;
+            }
+            else
+            {
+                return;
+            }
+        }
+    }
+}
---
+++ Zadanie 3/MultidimensionalDevice.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_3
+{
+    internal class MultidimensionalDevice : BaseDevice
+    {
+        Copier copier = new Copier();
+        Fax fax = new Fax();
+        public int ScanCounter => copier.ScanCounter;
+        public int PrintCounter => copier.PrintCounter;
+        public int FaxCounter => fax.FaxCounter;
+        public void Scan(out IDocument document, IDocument.FormatType formatType)
+        {
+            if (IDevice.State.on == state)
+            {
+                copier.PowerOn();
+                copier.Scan(out document, formatType);
+                copier.PowerOff();
+            }
+            else
+            {
+                document = null;
+                return;
+            }
+        }
+        public void Fax(in IDocument document, string recipient)
+        {
+            if(IDevice.State.on == state)
+            {
+                fax.PowerOn();
+                fax.SendFax(in  document,  recipient);
+                fax.PowerOff();
+            }
+        }
+
+        public void Print(in IDocument document)
+        {
+            if (IDevice.State.on == state)
+            {
+                copier.PowerOn();
+                copier.Print(document);
+                copier.PowerOff();
+            }
+            else
+            {
+                return;
+            }
+        }
+        public void ScanAndPrint()
+        {
+            if (IDevice.State.on == state)
+            {
+                copier.ScanAndPrint();
+            }
+            else
+            {
+                return;
+            }
+
+        }
+
+
+    }
+}
